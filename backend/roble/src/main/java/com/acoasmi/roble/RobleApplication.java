41fad10157bfb16e1b9e43f60631ab9da9f0c3e7
LR24003 <LR24003@ues.edu.sv>
2026-07-12 09:08:31 -0600

    Inicio de proyecto
    Se agrega toda la estructura del proyecto, iniciando con el backend mediante springboot.

---
+++ backend/roble/src/main/java/com/acoasmi/roble/RobleApplication.java
@@ -0,0 +1,13 @@
+package com.acoasmi.roble;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RobleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RobleApplication.class, args);
+	}
+
+}
---
+++ backend/roble/src/test/java/com/acoasmi/roble/RobleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.acoasmi.roble;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RobleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
